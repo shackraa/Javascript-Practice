013c082a5d52acbecc231dbae0f9bc243ad67bfa
Ben Atakan <95279470+shackraa@users.noreply.github.com>
2023-09-13 17:06:56 +0300

    Update THIS keyword.js

--- THIS keyword.js
+++ THIS keyword.js
@@ -1,1 +1,22 @@
-"use strict";
+'use strict';
+var firstName = "matilda";
+//This keyword usage;
+const jonas = {
+    year: 1991,
+    calcAge: function(){
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+
+    greet: () => {
+        console.log(this);
+        console.log(this.firstName); // var creates an object in global scope thats why :) another reason to not to use var
+    },
+    
+ };
+ const matilda = {
+    year : 2017,
+ };
+ matilda.calcAge = jonas.calcAge
+ matilda.calcAge();
+ jonas.greet();
