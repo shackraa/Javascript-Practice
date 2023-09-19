f6adbdacf2db124b803a91a7a8f90852ad4f5253
Ben Atakan <95279470+shackraa@users.noreply.github.com>
2023-09-19 19:42:41 +0300

    update script.js

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -30,10 +30,24 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu,[mainIndex]];
   },
   orderDelivery: function ({starterIndex, mainIndex, time, address}) {
-    console.log("Order received! ${this.starterM}")
-  },
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu,[mainIndex]} will be delivered to ${address} at ${time}`);
+},
 
 };
+
+restaurant.orderDelivery({
+  time:"22:30",
+  address:"Via del Sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address:"Via del Sole, 21",
+  starterIndex: 1, 
+});
+
+
 
 const {name, openingHours, categories} = restaurant;
 console.log(name, openingHours, categories);
